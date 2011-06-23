5d2861534ed85c6835aadbfa0d98f913fb51fbdf
Robert G. Jakabosky <bobby@sharedrealm.com>
2011-06-23 04:54:06 -0700

    Raw decode & dump script cleanup.

--- tests/decode_raw.lua
+++ tests/decode_raw.lua
@@ -2,16 +2,16 @@
 local pb = require"pb"
 local decode_msg = pb.decode
 
-local utils = require"utils"
+if #arg < 1 then
+	print("Usage: " .. arg[0] .. " <raw protobuf encoded message>")
+	return
+end
 
 local f = assert(io.open(arg[1]))
 local bin = assert(f:read('*a'))
 assert(f:close())
 
-print("--- decode raw message")
 local msg, off = decode_msg(nil, bin)
 
-print(utils.dump(msg))
-
-print("--- decoded raw message:", msg, off, #bin)
+pb.print(msg)
 
