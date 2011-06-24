7907fdd021dfeb546bcfa666dcf6d5206b03d024
Robert G. Jakabosky <bobby@sharedrealm.com>
2011-06-24 01:35:21 -0700

    Add simple person.proto example.

---
+++ example_person.lua
@@ -0,0 +1,20 @@
+-- load lua-pb first.
+require"pb"
+
+-- now you can use require to load person.proto
+local person = require"person"
+
+local msg = person.Person()
+msg.name = "John Doe"
+msg.id = 1234
+msg.email = "jdoe@example.com"
+pb.print(msg)
+
+print("Encode person message to binary.")
+local bin = pb.encode(msg)
+print("bytes =", #bin)
+
+print("Decode person message from binary.")
+local msg2 = pb.decode(person.Person(), bin)
+pb.print(msg2)
+
