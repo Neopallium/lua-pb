0f6d6432173d3665d56b3aef6636618489a4ab50
Robert G. Jakabosky <bobby@sharedrealm.com>
2011-08-04 23:23:56 -0700

    Correct media test data.

--- bench_media_pb.lua
+++ bench_media_pb.lua
@@ -21,8 +21,8 @@ local sample = {
 	media = {
 		uri = "http://javaone.com/keynote.mpg",
 		title = "Javaone Keynote",
-		width = 640,
-		height = 480,
+		height = 640,
+		width = 480,
 		format = "video/mpg4",
 		duration = 18000000,    -- half hour in milliseconds
 		size = 58982400,        -- bitrate * duration in seconds / 8 bits per byte
@@ -40,7 +40,7 @@ local sample = {
 			size = "LARGE",
 		},
 		{
-			uri = "http://javaone.com/keynote_small.jpg",
+			uri = "http://javaone.com/keynote_thumbnail.jpg",
 			title = "Javaone Keynote",
 			width = 320,
 			height = 240,
@@ -56,8 +56,8 @@ local function check_MediaContent_media(media)
 	assert(media ~= nil)
 	assert(media.uri == "http://javaone.com/keynote.mpg")
 	assert(media.title == "Javaone Keynote")
-	assert(media.width == 640)
-	assert(media.height == 480)
+	assert(media.width == 480)
+	assert(media.height == 640)
 	assert(media.format == "video/mpg4")
 	assert(media.duration == 18000000)    -- half hour in milliseconds
 	assert(media.size == 58982400)        -- bitrate * duration in seconds / 8 bits per byte
@@ -85,7 +85,7 @@ local function check_MediaContent(content)
 	assert(img.size == "LARGE")
 	img = image[2]
 	assert(img ~= nil)
-	assert(img.uri == "http://javaone.com/keynote_small.jpg")
+	assert(img.uri == "http://javaone.com/keynote_thumbnail.jpg")
 	assert(img.title == "Javaone Keynote")
 	assert(img.width == 320)
 	assert(img.height == 240)
@@ -96,14 +96,47 @@ local enc = { name="pb"}
 function enc:create()
 	return MediaContentHolder.MediaContent()
 end
-function enc:build(obj, data)
-	data = data or sample
+function enc:build(obj)
 	if not obj then
-		return MediaContentHolder.MediaContent(data)
+		return MediaContentHolder.MediaContent(sample)
 	end
-	-- fill old message.
-	obj.media = data.media
-	obj.image = data.image
+	obj:CopyFrom(sample)
+--[[
+	-- create media record.
+	local media = obj.media
+	if not media then obj.media = {}; media = obj.media end
+	media.format = "video/mpg4"
+	media.player = "JAVA"
+	media.title = "Javaone Keynote"
+	media.uri = "http://javaone.com/keynote.mpg"
+	media.duration = 18000000
+	media.size = 58982400
+	media.height = 640
+	media.width = 480
+	media.bitrate = 262144
+	-- add persons.
+	media.person = {}
+	local person = media.person
+	person:Add("Bill Gates")
+	person:Add("Steve Jobs")
+
+	-- create image records.
+	obj.image = {}
+	local image1 = obj.image:Add()
+	image1.height = 768
+	image1.title = "Javaone Keynote"
+	image1.uri = "http://javaone.com/keynote_large.jpg"
+	image1.width = 1024
+	image1.size = "LARGE"
+
+	local image2 = obj.image:Add()
+	image2.height = 240
+	image2.title = "Javaone Keynote"
+	image2.uri = "http://javaone.com/keynote_thumbnail.jpg"
+	image2.width = 320
+	image2.size = "SMALL"
+--]]
+
 	return obj
 end
 function enc:free(obj)
