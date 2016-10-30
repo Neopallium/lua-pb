f8dc4995330f8806fa0470984efd53e1a718b506
Robert G. Jakabosky <bobby@sharedrealm.com>
2016-10-30 20:56:18 +0800

    Fix big numbers decode test.

--- tests/test_big_numbers_decode.lua
+++ tests/test_big_numbers_decode.lua
@@ -16,31 +16,48 @@ local data = {
 	field5 = "\127\255\255\255\255\255\255\255",
 	field6 = 9876543210.9876543210,
 }
-local cdata = {
-	field1 = "9223372036854775807ULL",
-	field2 = "9223372036854775807ULL",
-	field3 = "9223372036854775807ULL",
-	field4 = "9223372036854775807ULL",
-	field5 = "9223372036854775807ULL",
-}
--- detect LuaJIT 2.x
-if jit then
-	local ffi = require'ffi'
-	if ffi then
-		-- load cdata.
-		for field,val in pairs(cdata) do
-			data[field] = loadstring("return 9223372036854775807ULL")()
-		end
+local make_int64 = pb.get_make_int64_func()
+for k,v in pairs(data) do
+	if type(v) == 'string' then
+		data[k] = make_int64(v:byte(1, 8))
 	end
 end
 
 print(utils.dump(data))
 
+local function to_hex(n)
+	local t = type(n)
+	if t ~= 'string' then
+		if t == 'number' then
+			return string.format('%016X', n)
+		else
+			return string.format('%08X%08X', tonumber(n / 0x100000000), tonumber(n % 0x100000000))
+		end
+	end
+	local l = #n
+	if l == 0 then return '0000000000000000' end
+	n = (n:gsub('.', utils.hex))
+	if l < 8 then
+		n = ('00'):rep(8 - l) .. n
+	end
+	return n
+end
+
+local function cmp_raw64(n1, n2)
+	if n1 == n2 then return true end
+	if to_hex(n1) == to_hex(n2) then return true end
+	return false
+end
+
 local function check_msg(msg)
 	assert(msg ~= nil)
 	for k,v in pairs(data) do
-		print('--- check:', k, msg[k])
-		assert(msg[k] == v)
+		local v1 = msg[k]
+		print('--- check:', k, to_hex(v1), to_hex(v), type(v1), type(v))
+		if not cmp_raw64(v1, v) then
+			error(string.format("field '%s' differs: %s ~= %s",
+				k, utils.dump(v1), utils.dump(v)))
+		end
 	end
 end
 
